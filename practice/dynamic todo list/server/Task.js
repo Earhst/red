ef5d70d7aa04fea137ab9700f90678b19317a3d4
Earhst <zekeyeager36@gmail.com>
2025-07-04 17:29:38 +0800

    removed all contents and added tagumpai folder

--- practice/dynamic todo list/server/Task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const taskSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  day: { type: String, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Task', taskSchema);
